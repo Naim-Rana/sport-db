ebbc44ceb6470bc0ffdfbda27a22bf244330215f
Naim-Rana <naimrana015@gmail.com>
2022-02-28 11:42:55 +0600

    add allPlayers array function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("hi")
+const allPlayers =async () => {
+    const searchValue = document.getElementById("search-box").value;
+    document.getElementById("search-box").value='';
+    const url =`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+    
+    const res =await fetch(url);
+    const data =await res.json();
+    console.log(data);
+    console.log(data.player);
+    console.log(data.player[0].strPlayer);
+}
